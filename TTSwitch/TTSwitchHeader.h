c0a6b1e511a332cbe024e56fb497f7522f938d20
Jordan Kay <jordanekay@twitter.com>
2015-02-24 18:08:33 -0800

    Add framework for Carthage support

---
+++ TTSwitch/TTSwitchHeader.h
@@ -0,0 +1,21 @@
+//
+//  TTSwitch.h
+//  TTSwitch
+//
+//  Created by Jordan Kay on 2/24/15.
+//  Copyright (c) 2015 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TTSwitch.
+FOUNDATION_EXPORT double TTSwitchFrameworkVersionNumber;
+
+//! Project version string for TTSwitch.
+FOUNDATION_EXPORT const unsigned char TTSwitchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTSwitch/PublicHeader.h>
+
+#import <TTSwitch/TTFadeSwitch.h>
+#import <TTSwitch/TTSwitch.h>
+#import <TTSwitch/TTSwitchSubclass.h>
